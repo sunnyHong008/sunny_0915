ff7d2760adae626354f7697a13b7d93f2cc9cb79
sunnyHong008 <331392318@qq.com>
2020-09-16 19:00:46 +0800

    add linkedBox

--- src/com/sunny/list/LinkedBox.java
+++ src/com/sunny/list/LinkedBox.java
@@ -1,26 +1,99 @@
 package com.sunny.list;
 
-public class LinkedBox implements Box{
-    private Node first;
-    private Node last;
+import com.sunny.exception.BoxIndexOutOfBoundsException;
 
-    @Override
-    public boolean add() {
+public class LinkedBox {
 
-        return false;
+    private Node first; //记录首节点
+    private Node last; //记录尾结点
+    private int size; //有效元素个数
+
+    /**
+     * 添加元素
+     * @param element
+     * @return
+     */
+    public boolean add(int element) {
+
+        this.linkLast(element);
+        return true;
     }
 
-    @Override
-    public int get() {
-        return 0;
+    //负责将新元素添加在新的node里，挂在尾端
+    private void linkLast(int element){
+
+        //获取链表的尾结点
+        Node l = last;
+        //创建一个新对象，将数据包装起来
+        Node newNode = new Node(l, element, null);
+        //将新节点对象设置为尾结点
+        last = newNode;
+        //首次
+        if(l==null){
+            first = newNode;
+        }else {
+            l.next = newNode;
+        }
+        size++;
     }
 
-    @Override
-    public int remove() {
-        return 0;
+    /**
+     * 获取指定元素
+     * @param index
+     * @return
+     */
+    public int get(int index){
+
+        this.checkIndex(index);
+     return this.node(index).item;
     }
 
-    @Override
+    private Node node(int index){
+        Node targetNode;
+        if(index < (size>>1)){//说明在前半部分，从头遍历效率高
+            targetNode = first;
+            for(int i = 0;i<index;i++){
+                targetNode = targetNode.next;
+            }
+        }else {//反之从尾遍历
+            targetNode = last;
+            for(int i = index-1;i>index;i--){
+                targetNode = targetNode.previous;
+            }
+        }
+        return targetNode;
+    }
+
+    //检测index合法性
+    private void checkIndex(int index){
+
+        if(index > size||index < 0){
+            throw new BoxIndexOutOfBoundsException("index不合法");
+        }
+    }
+
+    /**
+     * 删除指定元素
+     * @param index
+     * @return
+     */
+    public int remove(int index) {
+        this.checkIndex(index);
+        Node targetNode = this.node(index);
+        this.unlink(targetNode);
+        int oldValue = targetNode.item;
+
+        return oldValue;
+    }
+
+    //
+    private int unlink(Node targetNode){
+
+        int oldValue = targetNode.item;
+
+        return oldValue;
+    }
+
     public int size() {
         return 0;
     }
--- src/com/sunny/list/Node.java
+++ src/com/sunny/list/Node.java
@@ -1,10 +1,21 @@
 package com.sunny.list;
 
+/**
+ * 双向链表实体类
+ */
 public class Node {
 
-    private Node previous;
-    private int item;
-    private Node next;
+    public Node previous;//前一个节点
+    public int item;//所存数据
+    public Node next;//下一个节点
+
+    public Node(Node previous,int item,Node next){
+
+        this.previous = previous;
+        this.item = item;
+        this.next = next;
+
+    }
 
 
 }
